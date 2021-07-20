a51d6d653ab00ac527d4a37dfac6a3d17f5a7561
NiharikaNeha <45658446+NiharikaNeha@users.noreply.github.com>
2021-07-21 05:06:16 +0530

    ex06 call mock server

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var btnTranslate=document.querySelector('#button');
-var txtInput=document.querySelector("#input");
-var txtOutput=document.querySelector("#output");
+var btnTranslate = document.querySelector('#button');
+var txtInput = document.querySelector("#input");
+var txtOutput = document.querySelector("#output");
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getURL(text) {
+    return serverURL + "?text=" + text
+}
 
 function clickHandler() {
-    txtOutput.innerText = txtInput.value;
+
+    var input = txtInput.value;
+
+    fetch(getURL(input))
+        .then(response => response.json())
+        .then(json => {
+            txtOutput.innerText = json.contents.translated;
+        })
+
 }
 
 btnTranslate.addEventListener("click", clickHandler)
